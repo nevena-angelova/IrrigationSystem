b7ed9bdb751a34ae716edb9e6d938e08c8aca62f
nevena-angelova <nev.angelova@gmail.com>
2026-07-05 10:13:29 +0300

    irrigate mqtt message

--- src/main/java/irrigationsystem/service/MqttService.java
+++ src/main/java/irrigationsystem/service/MqttService.java
@@ -1,22 +1,29 @@
 package irrigationsystem.service;
 
-import irrigationsystem.dto.ResponseDto;
 import irrigationsystem.mqtt.MqttPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class MqttService {
     private final MqttPublisher mqttPublisher;
 
-    public ResponseDto<String> irrigate(long controllerId, int areaNumber, double duration) {
+    public void irrigate(long controllerId, int areaNumber, double duration) {
+
         String topic = "garden/" + controllerId + "/relay/" + areaNumber;
 
-        String payload = "ON:" + duration;
+        String payload = String.format(
+            Locale.US,
+            "{\"areaNumber\":%d,\"irrigationDuration\":%.2f}",
+            areaNumber,
+            duration
+        );
 
         mqttPublisher.publish(topic, payload);
-
-        return ResponseDto.<String>builder().value("Relay ON command sent to controller " + controllerId).build();
     }
 }
